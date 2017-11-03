7b4fcea0b32429763fb2e6aa6b8ad1d97d973244
Xabier Echezurieta <xabier3ee@gmail.com>
2017-11-03 11:56:18 +0100

    ej1 beta

--- insertarPregunta.php
+++ insertarPregunta.php
@@ -57,6 +57,8 @@
 			</footer>
 		</div>
 		<?php
+			
+
 
 			echo ("
 				<script type='text/javascript'>
@@ -81,6 +83,23 @@
 				
 
 				$sql = "INSERT INTO preguntas(email, enunciado, rCorrecta, rIncorrecta1, rIncorrecta2, rIncorrecta3, complejidad, tema, imagen) VALUES ('$_POST[email]', '$_POST[enunciado]', '$_POST[rCorrecta]', '$_POST[rIncorrecta1]', '$_POST[rIncorrecta2]', '$_POST[rIncorrecta3]', '$_POST[complejidad]', '$_POST[tema]', '$imagen')";
+
+				$xml = simplexml_load_file('preguntas.xml')
+
+				$registro = $xml->addChild('assessmentItem');
+				$registro->addAttribute('complexity', $_POST['complejidad']);
+				$registro->addAttribute('subject', $_POST['tema']);
+				$registro->addAttribute('author', $_POST['email']);
+				$registro->addChild('itemBody', $_POST['enunciado'].'\n');
+				$registro->addChild('correctResponse', $_POST['rCorrecta'].'\n');
+				$incorrecta1-> $registro->addChild('value', $_POST['rIncorrecta1'].'\n' );
+				$incorrecta2-> $registro->addChild('value', $_POST['rIncorrecta2'].'\n' );
+				$incorrecta3-> $registro->addChild('value', $_POST['rIncorrecta3'].'\n' );
+
+				echo $xml->asXML();
+				$xml->asXML('preguntas.xml');
+
+
 
 				if(!mysqli_query($link, $sql)) {
 					die("Error: " . mysqli_error($link));
